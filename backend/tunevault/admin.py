dbe99639fa7ebfc9f64754bdecb4c30dc224d084
Mica Perillo <micaelaperillo26@gmail.com>
2023-09-07 17:39:32 -0300

    models

--- backend/tunevault/admin.py
+++ backend/tunevault/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import Profile, Vault, Post, Comment
 
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(Vault)
+admin.site.register(Post)
+admin.site.register(Comment)
--- backend/tunevault/models.py
+++ backend/tunevault/models.py
@@ -1,3 +1,57 @@
 from django.db import models
+from django.contrib.auth import get_user_model
+import uuid
+from datetime import datetime
 
 # Create your models here.
+
+User = get_user_model()
+
+class Profile(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    id_user = models.IntegerField()
+    bio = models.TextField(blank=True)
+    profileimg = models.ImageField(upload_to='profile_image', default='profile_image.jpg')
+    location = models.CharField(max_length=100, blank=True)
+    # TODO guardar artistas/albumes favoritos
+
+    def __str__(self):
+        return self.user.username
+
+class Vault(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    title = models.TextField(max_length=150)
+    # TODO sacar mas info de la api de spotify
+    def __str__(self):
+        return self.title
+
+class Post(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.CharField(max_length=50)
+    # TODO relacionar las foreign keys
+    # TODO ver la longitud del id
+    vault_id = models.CharField(max_length=50)
+    likes = models.IntegerField(default=0)
+    date = models.DateField(default=datetime.now)
+    title = models.TextField(max_length=60)
+    content = models.TextField(max_length=597)
+
+    def __str__(self):
+        return self.user.username
+
+
+class Comment(models.Model):
+    id = models.UUIDField(primary_key=True, default=uuid.uuid4)
+    user = models.CharField(max_length=50)
+    # TODO relacionar las foreign keys
+    post_id = models.CharField(max_length=50)
+    comment_answer_id = models.CharField(max_length=50, default=0)
+    # si es respuesta directa al post -> 0
+    # si contesta a un comentario -> id del comentario y del post
+    # TODO mejorar comentarios de comentarios
+    likes = models.IntegerField(default=0)
+    date = models.DateField(default=datetime.now)
+    content = models.TextField(max_length=597)
+
+    def __str__(self):
+        return self.user.username
