bea343cc1d628a26038888ad1d277f91a6f0e551
agussang <msanguinetti@itba.edu.ar>
2023-09-11 12:30:44 -0300

    ROUTERDOM FIXED

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,30 +2,37 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './js/App';
-/*import Music from './js/Music.js';*/
+import Music from './js/Music.js';
 import reportWebVitals from './reportWebVitals';
-/*import { createBrowserRouter, RouterProvider,} from "react-router-dom";*/
+import { createBrowserRouter, RouterProvider,} from "react-router-dom";
 import "./index.css";
-
+/* 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
 );
-
-/* const router = createBrowserRouter([
+*/
+const router = createBrowserRouter([
   {
     path: "/",
-    element: <App/>,
+    element: <App/>,    
   },
+  [
+    {
+      path: "/Music",
+      element: <Music/>,
+      
+    },
+  ]
 ]);
 
-ReactDOM.createRoot(document.getElementById('root')).render(
+ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-); */
+); 
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
--- frontend/src/js/Components/Menu.js
+++ frontend/src/js/Components/Menu.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import '../../css/Components/Menu.css';
 import logo from '../../Resources/logo.png';
-/*import { Link } from "react-router-dom";*/
+import { Link } from "react-router-dom";
 
 export default function Menu() {
   return (
@@ -10,9 +10,10 @@ export default function Menu() {
         <img className='logo' src={logo} alt="logo" />
         <nav>
           <ul>
-            <li className='item'><a href="/Music">Music</a></li>
-            <li className='item'><a href="/Podcsats">Podcasts</a></li>
-            <li className='item'><a href="/LogIn">Log in</a></li>
+          <li><Link to="/">Home</Link></li>
+            <li><Link to="/Music">Music</Link></li>
+            <li><Link to="/Podcsats">Podcasts</Link></li>
+            <li><Link to="/LogIn">Log in</Link></li>
           </ul>
         </nav>
       </div>
@@ -20,8 +21,4 @@ export default function Menu() {
   );
 }
 
-/* <li><Link to="/">Home</Link></li>
-            <li><Link to="/Music">Music</Link></li>
-            <li><Link to="/Podcsats">Podcasts</Link></li>
-            <li><Link to="/LogIn">Log in</Link></li>
-            */
+            
