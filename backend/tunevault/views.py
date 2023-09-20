fb4539abf140437bca6e74aaf15df5cdf246a25b
Mica Perillo <micaelaperillo26@gmail.com>
2023-09-20 10:15:17 -0300

    signin, signup, settings

--- backend/tunevault/views.py
+++ backend/tunevault/views.py
@@ -2,7 +2,10 @@ from django.shortcuts import render
 from rest_framework import viewsets
 from .serializers import ProfileSerializer, PostSerializer, CommentSerializer, VaultSerializer
 from .models import Profile, Post, Comment, Vault
-
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User, auth
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
 # Create your views here.
 
 class ProfileView(viewsets.ModelViewSet):
@@ -21,17 +24,92 @@ class VaultView(viewsets.ModelViewSet):
     serializer_class = VaultSerializer
     queryset = Vault.objects.all()
 
+@login_required(login_url='signin')
 def settings(request):
-    pass
+    user_profile = Profile.objects.get(user=request.user)
+
+    if request.method == 'POST':
+        
+        if request.FILES.get('image') == None:
+            image = user_profile.profileimg
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        if request.FILES.get('image') != None:
+            image = request.FILES.get('image')
+            bio = request.POST['bio']
+            location = request.POST['location']
+
+            user_profile.profileimg = image
+            user_profile.bio = bio
+            user_profile.location = location
+            user_profile.save()
+        
+        return redirect('settings')
+    # return render(request, 'setting.html', {'user_profile': user_profile}) TODO llamar desde next js
+    return 0
 
 def profile(request):
     pass
 
 def signup(request):
-    pass
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        password2 = request.POST['password2']
+
+        if password == password2:
+            if User.objects.filter(email=email).exists():
+                messages.info(request, 'Email Taken')
+                return redirect('signup')
+            elif User.objects.filter(username=username).exists():
+                messages.info(request, 'Username Taken')
+                return redirect('signup')
+            else:
+                user = User.objects.create_user(username=username, email=email, password=password)
+                user.save()
+
+                #log user in and redirect to settings page
+                user_login = auth.authenticate(username=username, password=password)
+                auth.login(request, user_login)
+
+                #create a Profile object for the new user
+                user_model = User.objects.get(username=username)
+                new_profile = Profile.objects.create(user=user_model, id_user=user_model.id)
+                new_profile.save()
+                return redirect('settings')
+        else:
+            messages.info(request, 'Password Not Matching')
+            return redirect('signup')
+        
+    else:
+        #return render(request, 'signup.html') TODO llamar a next js desde aca
+        pass
 
 def signin(request):
-    pass
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+
+        user = auth.authenticate(username=username, password=password)
+
+        if user is not None:
+            auth.login(request, user)
+            return redirect('/')
+        else:
+            messages.info(request, 'Credentials Invalid')
+            return redirect('signin')
+
+    else:
+        # return render(request, 'signin.html') TODO llamar next js desde aca
+        pass 
 
 def vault(request):
     pass
