057c76b8027edabf4ae70481878e8df3fe9524c8
Mica Perillo <micaelaperillo26@gmail.com>
2023-10-06 20:02:03 -0300

    swapped urls.py my bad

--- backend/tunevault/urls.py
+++ backend/tunevault/urls.py
@@ -3,6 +3,12 @@ from . import views
 from django.contrib import admin
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('tunevault.urls')),
-    ]
+    #path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('settings/', views.settings_profile, name='settings'),
+    path('profile/<str:pk>', views.profile, name='profile'),
+    path('signup', views.signup, name='signup'),
+    path('signin', views.signin, name='signin'),
+    path('search', views.search_artist, name = 'search_artist'),
+    path('vault/<str:pk>', views.vault, name='vault'),
+]
