5bb9f14ac9c663e5a726fdc1afc4cdd3d2ace8da
Tim S <84805291+tm-sm@users.noreply.github.com>
2023-11-25 17:02:08 -0300

    adds search_member

--- backend/tunevault/utils.py
+++ backend/tunevault/utils.py
@@ -19,26 +19,44 @@ def get_artist(id):
 
 #se podrían pasar codigos de error para que el front los maneje
 def get_result_search(search, type, limit, offset, genre = None):
-    if ((type != 'artist' and type !="album" and type != "playlist" and type !="track" and type !="show" and type != "episode" and type !="audiobook") or limit < 0 or offset < 0):
+    if ((type != 'artist' and type !="album" and type != "playlist" and type !="track" and type !="show" and type != "episode" and type !="audiobook" and type !="member") or limit < 0 or offset < 0):
         return json.dumps({'error': 'Tipo de busqueda no valida'})
-    result = sp.search(search,limit,offset,type)
-    listToRet = []
-    if result[type]['items']==[]:
-        return json.dumps({'error': 'No se encontraron artistas'})
-    for items in result['artists']['items']:
-        if (genre != None):
-            if (genre in items['genres']):
+    
+    elif(type == "member"):
+        listToRet = []
+        total = 0
+        for profile in Profile.objects:
+            if profile.isArtist and (search in profile.user.get_username()):
+                listToRet.append(profile.user.get_username())
+                total += 1
+        
+        jsonResult = {
+            'type': type,
+            'members': listToRet,
+            'total': total,
+            'next': False,
+        }
+
+    else:
+        result = sp.search(search,limit,offset,type)
+        listToRet = []
+        if result[type]['items']==[]:
+            return json.dumps({'error': 'No se encontraron artistas'})
+        for items in result['artists']['items']:
+            if (genre != None):
+                if (genre in items['genres']):
+                    queryResult = get_or_create_vault(items)
+                    listToRet.append(queryResult)
+            else:
                 queryResult = get_or_create_vault(items)
                 listToRet.append(queryResult)
-        else:
-            queryResult = get_or_create_vault(items)
-            listToRet.append(queryResult)
-    jsonResult = {
-        'type': type,
-        'vaults': listToRet,
-        'total': result[type]['total'],
-        'next': result[type]['next'],
-    }
+        jsonResult = {
+            'type': type,
+            'vaults': listToRet,
+            'total': result[type]['total'],
+            'next': result[type]['next'],
+        }
+
     return jsonResult
 
 #para sección de música
@@ -55,13 +73,19 @@ def search_podcast(query):
     result = {searchPodcast, searchEpisode}
     return result
 
+def search_member(query):
+    searchMember = get_result_search(query, 'member', 10, 0)
+    result = {searchMember}
+    return result
+
 #para barra de navegación
 def search_all(query):
     searchArtist = get_result_search(query, 'artist', 10, 0)
     searchAlbum = get_result_search(query, 'album', 10, 0)
     searchPodcast = get_result_search(query, 'show', 10, 0)
     searchEpisode = get_result_search(query, 'episode', 10, 0)
-    result = {searchArtist, searchAlbum, searchPodcast, searchEpisode}
+    searchMember = get_result_search(query, 'member', 10, 0)
+    result = {searchArtist, searchAlbum, searchPodcast, searchEpisode, searchMember}
     return result
 
 def get_or_create_vault(item):
