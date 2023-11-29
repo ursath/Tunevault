ce3149a776836af22d38c11f0d954d2b3b67f209
UrielArias <urielarias.a12@gmail.com>
2023-11-29 14:07:15 -0300

    adding wsgi app

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -143,6 +143,7 @@ USE_I18N = True
 
 USE_TZ = True
 
+WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
